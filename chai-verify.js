7a2d04ddadcff4073381d8eabaab29df0ca2af75
Chris Stead <cmstead@gmail.com>
2019-07-26 13:52:03 -0700

    Added docs and normalizing behaviors

--- chai-verify.js
+++ chai-verify.js
@@ -1,5 +1,5 @@
 (function (verifyFactory) {
-    if(typeof module !== 'undefined' && module.exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports !== 'undefined') {
         module.exports = verifyFactory
     } else {
         chai.use(verifyFactory);
@@ -7,33 +7,87 @@
             utils: verifyFactory.utils
         };
     }
-    
+
 })(
     (function () {
+        function isTypeOf(type) {
+            return function (value) {
+                return typeof value === type;
+            }
+        }
+
+        const isFunction = isTypeOf('function');
+        const isObject = isTypeOf('object');
+        const isString = isTypeOf('string');
+
+        function functionExpectationByName(name = 'anonymous') {
+            return `[Function: ${name}]`;
+        }
+
+        function functionExpectationAsAnonymous() {
+            return functionExpectationByName('anonymous');
+        }
+
+        function functionExpectationAsNamed(name) {
+            if (!isString(name)) {
+                throw new Error('functionExpectationAsNamed requires function name to be a string');
+            }
+
+            return functionExpectationByName(name);
+        }
+
+        function normalizeValue(value, normalization) {
+            return isFunction(normalization)
+                ? normalization(value)
+                : normalization;
+        }
+
+        function normalizeValues(values, normalization) {
+            return values.map(value => normalizeValue(value, normalization));
+        }
+
+        function normalizeProperties(valueUnderTest, randomProperties) {
+            if ((!isObject(valueUnderTest) && !isFunction(valueUnderTest)) || valueUnderTest === null) {
+                throw new Error(`normalizeRandomValues expects valueUnderTest to be an object, but received ${valueUnderTest} of type ${typeof valueUnderTest}`);
+            }
+
+            if (!isObject(randomProperties) || randomProperties === null) {
+                throw new Error(`normalizeRandomValues expects randomProperties to be an object, but got ${randomProperties} of type ${typeof randomProperties}`);
+            }
+
+            Object
+                .keys(randomProperties)
+                .forEach(function (key) {
+                    const originalValue = valueUnderTest[key];
+                    const normalization = randomProperties[key];
+                    const normalizedValue = normalizeValue(originalValue, normalization);
+
+                    valueUnderTest[key] = normalizedValue;
+                });
+
+            return valueUnderTest;
+        }
+
         function verifyFactory(chai) {
 
-            function isFunction(value) {
-                return typeof value === 'function';
-            }
-        
             function handleFunctionProperties(key, value) {
-                if(isFunction(value)) {
+                if (isFunction(value)) {
                     const functionName = Boolean(value.name)
                         ? value.name
                         : 'anonymous';
-        
-                    return `[Function: ${functionName}]`;
+
+                    return functionExpectationByName(functionName);
                 }
-        
+
                 return value;
             }
-        
+
             function prettyJson(value) {
                 return typeof value === 'undefined'
                     ? 'undefined'
                     : JSON.stringify(value, handleFunctionProperties, 4);
             }
-        
+
             function errorBuilder(actualString, expectedString) {
                 return [
                     '\n',
@@ -49,36 +103,62 @@
                     expectedString
                 ].join('\n');
             }
-        
-            function verify(actual, expected) {
+
+            function verify(actual, expected, userMessage) {
                 const actualString = prettyJson(actual);
                 const expectedString = prettyJson(expected);
                 const errorMessage = errorBuilder(actualString, expectedString);
-        
+                const errorOutput = Boolean(userMessage)
+                    ? userMessage
+                    : errorMessage;
+
                 chai.assert(
                     actualString === expectedString,
-                    errorMessage,
+                    errorOutput,
                     errorMessage,
                     expected,
                     actual
                 );
             }
-        
+
+            function normalizeAndVerify(
+                actual,
+                expected,
+                normalizationProperties,
+                userMessage
+            ) {
+                const normalizedValueUnderTest = normalizeProperties(actual, normalizationProperties);
+
+                verify(normalizedValueUnderTest, expected, userMessage);
+            }
+
             Object.defineProperty(chai.assert, 'verify', {
                 writable: false,
                 value: verify
             });
-        
-            chai.Assertion.addChainableMethod('verifiedAs', function (expected){
-                verify(this._obj, expected);
+
+            Object.defineProperty(chai.assert, 'normalizeAndVerify', {
+                writable: false,
+                value: normalizeAndVerify
             });
-        
+
+            chai.Assertion.addChainableMethod('verifiedAs', function (expected, userMessage) {
+                verify(this._obj, expected, userMessage);
+            });
+
+            chai.Assertion.addChainableMethod('normalizedAndVerifiedAs', function (expected, normalizationProperties, userMessage) {
+                normalizeAndVerify(this._obj, expected, normalizationProperties, userMessage);
+            });
+
         }
 
         verifyFactory.utils = {
-            functionExpectationByName(name = 'anonymous'){
-                return `[Function: ${name}]`;
-            }
+            functionExpectationByName,
+            functionExpectationAsNamed,
+            functionExpectationAsAnonymous,
+            normalizeProperties,
+            normalizeValue,
+            normalizeValues
         };
 
         return verifyFactory;
